47b5e550b4aa0a5d2c6cea06e1c33f2408c4b765
arda <ardaklc0@gmail.com>
2024-04-24 18:47:21 +0300

    User can add up to 7 vectors.

--- main.cpp
+++ main.cpp
@@ -9,59 +9,52 @@
 #include "custom_raylib.h"
 #include <string>
 using namespace std;
+#define MAX_INPUT_CHARS_X 2
+#define MAX_INPUT_CHARS_Y 2
 
-#define MAX_INPUT_CHARS_X 1
-#define MAX_INPUT_CHARS_Y 1
+
+Vector2Particle particle1 = Vector2Particle(Vector2D(0, 0), 0);
+Vector2Particle particle2 = Vector2Particle(Vector2D(0, 0), 0);
+Vector2Particle particle3 = Vector2Particle(Vector2D(0, 0), 0);
+Vector2Particle particle4 = Vector2Particle(Vector2D(0, 0), 0);
+Vector2Particle particle5 = Vector2Particle(Vector2D(0, 0), 0);
+Vector2Particle particle6 = Vector2Particle(Vector2D(0, 0), 0);
+Vector2Particle particle7 = Vector2Particle(Vector2D(0, 0), 0);
+Vector2D R = particle1.getPosition() + particle2.getPosition() + particle3.getPosition() + particle4.getPosition() + particle5.getPosition() + particle6.getPosition() + particle7.getPosition();
+Vector2Particle particleR = Vector2Particle(R, 0);
+
+
 int main() {
 	const int screenWidth = 650;
 	const int screenHeight = 650;
+	
 	InitWindow(screenWidth, screenHeight, "Test");
-	char nameX[MAX_INPUT_CHARS_X + 1] = "\0";      // NOTE: One extra space required for null terminator char '\0'
-	char nameY[MAX_INPUT_CHARS_Y + 1] = "\0";      // NOTE: One extra space required for null terminator char '\0'
+	
+	char nameX[MAX_INPUT_CHARS_X + 1] = "\0";
+	char nameY[MAX_INPUT_CHARS_Y + 1] = "\0";
+	
 	int letterCountX = 0;
 	int letterCountY = 0;
+	
 	Rectangle textBoxX = { 545, 10, 25, 25 };
 	Rectangle textBoxY = { 585, 10, 25, 25 };
 	Rectangle submitButton = { 500, 40, 75, 25 };
+	Rectangle deleteButton = { 500, 70, 75, 25 };
+
 	bool mouseOnSubmitButton = false;
+	bool mouseOnDeleteButton = false;
 	bool mouseOnTextX = false;
 	bool mouseOnTextY = false;
+
 	int framesCounterX = 0;
 	int framesCounterY = 0;
 	int framesCounterSubmitButton = 0;
-	bool drawVector4 = false;
+	int framesCounterDeleteButton = 0;
+
+	int vectorNumber = 1;
+
 	SetTargetFPS(60);
 
-
-	Vector2Particle particle1 = Vector2Particle(Vector2D(-3, 4), 250);
-	Vector2Particle particle2 = Vector2Particle(Vector2D(1, 0), 50);
-	Vector2Particle particle3 = Vector2Particle(Vector2D(0, -2), 120);
-	float angle1 = particle1.getAngle();
-	float angle2 = particle2.getAngle();
-	float angle3 = particle3.getAngle();
-	float Rx = particle1.getXForce() + particle2.getXForce() + particle3.getXForce();
-	float Ry = particle1.getYForce() + particle2.getYForce() + particle3.getYForce();
-	Vector2D R = particle1.getPosition() + particle2.getPosition() + particle3.getPosition();
-	Vector2D ForceR = Vector2D(Rx, Ry);
-	float RMagnitude = ForceR.magnitude();
-	float angle4 = ForceR.angleBetweenVectorsInDegrees();
-	cout << "Angle = " << angle1 << endl;
-	cout << "(Fx1, Fy1) = " << "(" << particle1.getXForce() << ", " << particle1.getYForce() << ")" << endl;
-	cout << "Angle = " << angle2 << endl;
-	cout << "(Fx2, Fy2) = " << "(" << particle2.getXForce() << ", " << particle2.getYForce() << ")" << endl;
-	cout << "Angle = " << angle3 << endl;
-	cout << "(Fx3, Fy3) = " << "(" << particle3.getXForce() << ", " << particle3.getYForce() << ")" << endl;
-	cout << "(Rx, Ry) = " << "(" << R.x << ", " << R.y << ")" << endl;
-	cout << "|R| = " << RMagnitude << endl;
-	cout << "Angle: " << angle4 << endl;
-	float normalizedForce1 = (particle1.getNewton() - 50) / 250 + 1;
-	cout << "Normalized Force 1: " << normalizedForce1 << endl;
-	float normalizedForce2 = (particle2.getNewton() - 50) / 250 + 1;
-	cout << "Normalized Force 2: " << normalizedForce2 << endl;
-	float normalizedForce3 = (particle3.getNewton() - 50) / 250 + 1;
-	cout << "Normalized Force 3: " << normalizedForce3 << endl;
-	float normalizedForceR = (RMagnitude - 50) / 250 + 1;
-	cout << "Normalized Force R: " << normalizedForceR << endl;
 
 	while (!WindowShouldClose())
 	{
@@ -127,6 +120,7 @@ int main() {
 		else framesCounterY = 0;
 
 		// ---------------------------------------------------------
+
 		if (CheckCollisionPointRec(GetMousePosition(), submitButton)) mouseOnSubmitButton = true;
 		else mouseOnSubmitButton = false;
 		if (mouseOnSubmitButton)
@@ -134,32 +128,135 @@ int main() {
 			SetMouseCursor(MOUSE_CURSOR_POINTING_HAND);
 			if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
 			{
-				cout << "Submit button pressed" << endl;
-				drawVector4 = true;
+				switch (vectorNumber) {
+					case 1: {
+						int posX = std::stoi(nameX);
+						int posY = std::stoi(nameY);
+						particle1 = Vector2Particle(Vector2D(posX, posY), 100);
+						R = particle1.getPosition() + particle2.getPosition() + particle3.getPosition() + particle4.getPosition() + 
+							particle5.getPosition() + particle6.getPosition() + particle7.getPosition();
+						particleR = Vector2Particle(R, 0);
+						vectorNumber++;
+						break;
+					}
+					case 2: {
+						int posX = std::stoi(nameX);
+						int posY = std::stoi(nameY);
+						particle2 = Vector2Particle(Vector2D(posX, posY), 100);
+						R = particle1.getPosition() + particle2.getPosition() + particle3.getPosition() + particle4.getPosition() +
+							particle5.getPosition() + particle6.getPosition() + particle7.getPosition();
+						particleR = Vector2Particle(R, 0);
+						vectorNumber++;
+						break;
+					}
+					case 3: {
+						int posX = std::stoi(nameX);
+						int posY = std::stoi(nameY);
+						particle3 = Vector2Particle(Vector2D(posX, posY), 100);
+						R = particle1.getPosition() + particle2.getPosition() + particle3.getPosition() + particle4.getPosition() + 
+							particle5.getPosition() + particle6.getPosition() + particle7.getPosition();
+						particleR = Vector2Particle(R, 0);
+						vectorNumber++;
+						break;
+					}
+					case 4: {
+						int posX = std::stoi(nameX);
+						int posY = std::stoi(nameY);
+						particle3 = Vector2Particle(Vector2D(posX, posY), 100);
+						R = particle1.getPosition() + particle2.getPosition() + particle3.getPosition() + particle4.getPosition() +
+							particle5.getPosition() + particle6.getPosition() + particle7.getPosition();
+						particleR = Vector2Particle(R, 0);
+						vectorNumber++;
+						break;
+					}
+					case 5: {
+						int posX = std::stoi(nameX);
+						int posY = std::stoi(nameY);
+						particle3 = Vector2Particle(Vector2D(posX, posY), 100);
+						R = particle1.getPosition() + particle2.getPosition() + particle3.getPosition() + particle4.getPosition() + 
+							particle5.getPosition() + particle6.getPosition() + particle7.getPosition();
+						particleR = Vector2Particle(R, 0);
+						vectorNumber++;
+						break;
+					}
+					case 6: {
+						int posX = std::stoi(nameX);
+						int posY = std::stoi(nameY);
+						particle3 = Vector2Particle(Vector2D(posX, posY), 100);
+						R = particle1.getPosition() + particle2.getPosition() + particle3.getPosition() + particle4.getPosition() + 
+							particle5.getPosition() + particle6.getPosition() + particle7.getPosition();
+						particleR = Vector2Particle(R, 0);
+						vectorNumber++;
+						break;
+					}
+					case 7: {
+						int posX = std::stoi(nameX);
+						int posY = std::stoi(nameY);
+						particle3 = Vector2Particle(Vector2D(posX, posY), 100);
+						R = particle1.getPosition() + particle2.getPosition() + particle3.getPosition() + particle4.getPosition() + 
+							particle5.getPosition() + particle6.getPosition() + particle7.getPosition();
+						particleR = Vector2Particle(R, 0);
+						vectorNumber++;
+						break;
+					}
+				}
+			}
+		}
+
+		if (CheckCollisionPointRec(GetMousePosition(), deleteButton)) mouseOnDeleteButton = true;
+		else mouseOnDeleteButton = false;
+		if (mouseOnDeleteButton) {
+			if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+				nameX[0] = '\0';
+				nameY[0] = '\0';
+				letterCountX = 0;
+				letterCountY = 0;
+				vectorNumber = 1;
+				particle1 = Vector2Particle(Vector2D(0, 0), 0);
+				particle2 = Vector2Particle(Vector2D(0, 0), 0);
+				particle3 = Vector2Particle(Vector2D(0, 0), 0);
+				particle4 = Vector2Particle(Vector2D(0, 0), 0);
+				particle5 = Vector2Particle(Vector2D(0, 0), 0);
+				particle6 = Vector2Particle(Vector2D(0, 0), 0);
+				particle7 = Vector2Particle(Vector2D(0, 0), 0);
+				R = particle1.getPosition() + particle2.getPosition() + particle3.getPosition() + particle4.getPosition() + 
+					particle5.getPosition() + particle6.getPosition() + particle7.getPosition();
+				particleR = Vector2Particle(R, 0);
 			}
 		}
 
 
 		BeginDrawing();
+
 		ClearBackground(BLACK);
 		CustomRaylib::PlotGrids(screenWidth, screenHeight);
-		CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, particle1.getXPosition(), particle1.getYPosition(), PINK, normalizedForce1);
+
+
+		CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, particle1.getXPosition(), particle1.getYPosition(), PINK, 1);
 		CustomRaylib::WriteLegend(10, 10, "Vector 1", PINK);
-		CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, particle2.getXPosition(), particle2.getYPosition(), RED, normalizedForce2);
+		
+		CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, particle2.getXPosition(), particle2.getYPosition(), RED, 1);
 		CustomRaylib::WriteLegend(10, 30, "Vector 2", RED);
-		CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, particle3.getXPosition(), particle3.getYPosition(), ORANGE, normalizedForce3);
+		
+		CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, particle3.getXPosition(), particle3.getYPosition(), ORANGE, 1);
 		CustomRaylib::WriteLegend(10, 50, "Vector 3", ORANGE);
-		CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, R.x, R.y, GREEN, normalizedForceR);
-		CustomRaylib::WriteLegend(10, 70, "Vector R", BLUE);
-		if (drawVector4) {
-			int posX = std::stoi(nameX);
-			int posY = std::stoi(nameY);
-			Vector2Particle particle4 = Vector2Particle(Vector2D(posX, posY), 100);
-			cout << particle4.getXPosition() << endl;
-			cout << particle4.getYPosition() << endl;
-			CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, particle4.getXPosition(), particle4.getYPosition(), PINK, 1);
-			CustomRaylib::WriteLegend(10, 90, "Vector 4", PINK);
-		}
+
+		CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, particle3.getXPosition(), particle4.getYPosition(), BLUE, 1);
+		CustomRaylib::WriteLegend(10, 70, "Vector 4", BLUE);
+
+		CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, particle3.getXPosition(), particle5.getYPosition(), YELLOW, 1);
+		CustomRaylib::WriteLegend(10, 90, "Vector 5", YELLOW);
+
+		CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, particle3.getXPosition(), particle6.getYPosition(), GRAY, 1);
+		CustomRaylib::WriteLegend(10, 110, "Vector 6", GRAY);
+
+		CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, particle3.getXPosition(), particle7.getYPosition(), VIOLET, 1);
+		CustomRaylib::WriteLegend(10, 130, "Vector 7", VIOLET);
+	
+		
+		
+		CustomRaylib::PlotThickOriginVector2D(screenWidth, screenHeight, R.x, R.y, GREEN, 1);
+		CustomRaylib::WriteLegend(10, 150, "Vector R", GREEN);
 		
 		DrawRectangleRec(textBoxX, LIGHTGRAY);
 		if (mouseOnTextX) DrawRectangleLines((int)textBoxX.x, (int)textBoxX.y, (int)textBoxX.width, (int)textBoxX.height, RED);
@@ -175,6 +272,11 @@ int main() {
 		if (mouseOnSubmitButton) DrawRectangleLines((int)submitButton.x, (int)submitButton.y, (int)submitButton.width, (int)submitButton.height, RED);
 		else DrawRectangleLines((int)submitButton.x, (int)submitButton.y, (int)submitButton.width, (int)submitButton.height, DARKGRAY);
 		DrawTextEx(GetFontDefault(), "Submit", {submitButton.x + 12, submitButton.y + 8}, 10, 5, MAROON);
+
+		DrawRectangleRec(deleteButton, LIGHTGRAY);
+		if (mouseOnDeleteButton) DrawRectangleLines((int)deleteButton.x, (int)deleteButton.y, (int)deleteButton.width, (int)deleteButton.height, RED);
+		else DrawRectangleLines((int)deleteButton.x, (int)deleteButton.y, (int)deleteButton.width, (int)deleteButton.height, DARKGRAY);
+		DrawTextEx(GetFontDefault(), "Delete", {deleteButton.x + 12, deleteButton.y + 8}, 10, 5, MAROON);
 
 		EndDrawing();
 	}
